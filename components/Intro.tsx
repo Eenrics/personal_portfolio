c6dd3fec3db6a276ef2dffa27c73922567909383
Eenrics <ebenezeresh525@gmail.com>
2023-04-22 17:06:37 +0300

    problems with svg fixd

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -2,7 +2,7 @@ const Intro = () => {
     return ( 
         <div className='text-white w-screen flex flex-col justify-center '>
             <p className='ml-auto mr-auto text-2xl'>Hi, I am {" "}<br/>
-              <span className='text-5xl text-white font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500'>Abenezer Eshetie</span>,
+              <span className='text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500'>Abenezer Eshetie</span>,
               <br/> a web developer who speaks the language of the internet and turns ideas into reality.</p>
             <br/>
             <p className='w-8/12 mr-auto ml-auto bg-white text-black p-5 rounded text-l'>I am a versatile web and mobile application developer with expertise in front-end, back-end, and DevOps..
--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -10,14 +10,14 @@ const NavigationBar = () => {
                     xmlns="http://www.w3.org/2000/svg"
                     xmlnsXlink="http://www.w3.org/1999/xlink"
                     viewBox="0 0 300 300"
-                    shape-rendering="geometricPrecision"
-                    text-rendering="geometricPrecision">
-                      <defs><linearGradient id="ePBXlpJ48592-fill" x1="0" y1="0.5" x2="1" y2="0.5" spreadMethod="pad" gradientUnits="objectBoundingBox" gradientTransform="translate(0 0)"><stop id="ePBXlpJ48592-fill-0" offset="0%" stop-color="#fff"/><stop id="ePBXlpJ48592-fill-1" offset="100%" stop-color="#f4f4f4"/></linearGradient></defs>
+                    shapeRendering="geometricPrecision"
+                    textRendering="geometricPrecision">
+                      <defs><linearGradient id="ePBXlpJ48592-fill" x1="0" y1="0.5" x2="1" y2="0.5" spreadMethod="pad" gradientUnits="objectBoundingBox" gradientTransform="translate(0 0)"><stop id="ePBXlpJ48592-fill-0" offset="0%" stopColor="#fff"/><stop id="ePBXlpJ48592-fill-1" offset="100%" stopColor="#f4f4f4"/></linearGradient></defs>
                       <path d="M52.189772,217.51824C61.678823,205.109481,163.868613,48.905109,170.437956,43.79562s11.678832-5.839417,16.058394-1.459855s20.437957,150.364964,20.437957,156.20438.729927,5.109489,0,13.868613-2.91971,32.846715-18.978104,21.89781-54.014606-55.474459-54.014606-60.583948c0-10.218978-4.379562-18.248175,6.569343-13.138686q5.109489,5.839416,47.445263,51.824824-16.058393-108.029204-17.518247-113.86862c-1.459854-5.839416-9.48906-9.489051-13.138695,0-7.29927,10.218978-90.510949,126.277372-96.350365,129.19708-10.948905,6.56935-14.598533,1.459862-8.759124-10.218978Z"
                         transform="translate(.000009 0.000008)"
                         fill="url(#ePBXlpJ48592-fill)"
                         stroke="#3f5787"
-                        stroke-width="0.6"
+                        strokeWidth="0.6"
                         />
               </svg>
             </div>
