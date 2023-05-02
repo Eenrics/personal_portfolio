9f75ad7b775ca84ac15c3e181d44acecf54c91ae
Eenrics <ebenezeresh525@gmail.com>
2023-05-02 14:50:48 +0300

    still on prog

--- components/About.tsx
+++ components/About.tsx
@@ -3,15 +3,13 @@ import Image from 'next/image'
 const About = () => {
 
     return ( 
-        <div className="flex flex-col w-screen h-screen py-36 pl-12" id='about'>
+        <div className="flex flex-col w-screen py-36 pl-12" id='about'>
           
-          <div className='flex flex-row gap-9'>
-            <div className='border-2 h-72 border-cyan-600 bg-cyan-900 hover:bg-green-900 hover:border-white rounded'>
-                <Image
+          <div className='flex flex-row gap-9 justify-center items-center'>
+            <div className='border-2 border-black bg-black hover:bg-cyan-900 hover:border-white rounded-full overflow-hidden '>
+                <img
                 src="/my_picture.png"
                 alt="abenezer picture"
-                width={200}
-                height={200}
                 style={{height: 285, width: 500}}
                 />
             </div>
@@ -27,24 +25,49 @@ const About = () => {
             </div>
           </div>
 
-          <div className='flex flex-row gap-4 font-[600] text-xl rounded-xl w-11/12 bg-gray-200 h-14 m-4 mt-16 p-3'>
-            <span>React</span>
-            <span>Redux</span>
-            <span>Next</span>
-            <span>Tailwind</span>
-            <span>Express</span>
-            <span>MongoDB</span>
-            <span>Flask</span>
-            <span>GraphQL</span>
-            <span>React Native</span>
-            <span>Kubernetes</span>
-            <span>Docker</span>
-            <span>Jenkins</span>
-            <span>Ansible</span>
-            <span>Prometheus</span>
-            <span>Terraform</span>
-            <span>AWS Cloud</span>
-          </div>
+
+            <div className=' bg-white grid grid-cols-12 gap-8 rounded-lg m-4 mt-16 ml-24 w-10/12 p-3 filter grayscale hover:grayscale-0 hover:scale-105 ease-out transition'>
+              <img src="/assets/logo/react.png" alt="heart icon" className='hover:animate-spin'/>
+              <img src="/assets/logo/three.png" alt="heart icon" />
+              <img src="/assets/logo/tailwind.png" alt="heart icon" />
+              <img src="/assets/logo/sass.png" alt="heart icon" />
+              <img src="/assets/logo/redux.png" alt="heart icon" />
+              <img src="/assets/logo/ansible.png" alt="heart icon" />
+              <img src="/assets/logo/aws.png" alt="heart icon" />
+              <img src="/assets/logo/bootstrap.png" alt="heart icon" />
+              <img src="/assets/logo/c.png" alt="heart icon" />
+              <img src="/assets/logo/python.png" alt="heart icon" />
+              <img src="/assets/logo/circleci.png" alt="heart icon" />
+              <img src="/assets/logo/css.png" alt="heart icon" />
+              <img src="/assets/logo/cypress.png" alt="heart icon" />
+              <img src="/assets/logo/docker.png" alt="heart icon" />
+              <img src="/assets/logo/express.png" alt="heart icon" />
+              <img src="/assets/logo/flask.png" alt="heart icon" />
+              <img src="/assets/logo/framer_motion.png" alt="heart icon" />
+              <img src="/assets/logo/graphana.png" alt="heart icon" />
+              <img src="/assets/logo/graphql.png" alt="heart icon" />
+              <img src="/assets/logo/grcp.png" alt="heart icon" />
+              <img src="/assets/logo/haproxy.png" alt="heart icon" />
+              <img src="/assets/logo/html.png" alt="heart icon" />
+              <img src="/assets/logo/javascript.png" alt="heart icon" />
+              <img src="/assets/logo/jenkins.png" alt="heart icon" />
+              <img src="/assets/logo/jest.png" alt="heart icon" />
+              <img src="/assets/logo/kubernetes.png" alt="heart icon" />
+              <img src="/assets/logo/mongodb.png" alt="heart icon" />
+              <img src="/assets/logo/mysql.png" alt="heart icon" />
+              <img src="/assets/logo/neo4j.png" alt="heart icon" />
+              <img src="/assets/logo/next.png" alt="heart icon" />
+              <img src="/assets/logo/nginx.png" alt="heart icon" />
+              <img src="/assets/logo/node.png" alt="heart icon" />
+              <img src="/assets/logo/postgresql.png" alt="heart icon" />
+              <img src="/assets/logo/prometheus.png" alt="heart icon" />
+              <img src="/assets/logo/react_native.png" alt="heart icon" />
+              <img src="/assets/logo/redis.png" alt="heart icon" />
+              <img src="/assets/logo/socketio.png" alt="heart icon" />
+              <img src="/assets/logo/terraform.png" alt="heart icon" />
+              <img src="/assets/logo/typescript.png" alt="heart icon" />
+              <img src="/assets/logo/webrtc.png" alt="heart icon" />
+            </div>
         </div>
      );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Intro from '@/components/Intro'
 import NavigationBar from '@/components/NavigationBar'
 import Abount from '@/components/About'
-
+import Works from '@/components/Works'
 
 export default function Home() {
   return (
@@ -16,6 +16,7 @@ export default function Home() {
         <main className="min-h-screen bg-black overflow-x-hidden">
           <Intro />
           <Abount />
+          <Works />
         </main>
     </>
   )
